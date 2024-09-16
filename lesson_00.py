f3684dcb99795ae9f4a1fc514d6982090a00801b
Hasain Jinan <hjinan@brooklynsteamcenter.org>
2024-09-16 10:48:01 -0400

    Lesson 00

---
+++ lesson_00.py
@@ -0,0 +1,42 @@
+import random
+#Step One: Create File
+
+#Step Two: Print Name.
+print("Hasain Jinan")
+
+#Step Three: Install Python
+
+#Step Four: Create var for favorite show.
+fav_show = "One Punch Man"
+
+#Step Five: Create one float and one integer variable.
+int = int(2)
+float = float(1.5)
+
+#Step Six: Print the product of the above variables.
+print(int * float)
+
+#Step Seven: Create a variable that stores your favorite foods.
+fav_foods = ["Pizza", "Ceasar Salad", "Biryani"]
+
+#Step Eight: Create an if statement to see if someone can get a driver's license.
+age = random.randrange(1, 100)
+if age >= 18:
+    print("You are eligible for a driver's license.")
+else:
+    print("You are not eligible fora driver's license.")
+
+#Step Nine: Ask for a name and print it to the console.
+name = input("What is your name?")
+print(name)
+
+#Step Ten: Create a for loop that prints your favorite foods.
+for x in fav_foods:
+    print(x)
+
+#Step Eleven: Create a while loop that doesnt run.
+num = 0
+while num == 1:
+    print("This is broken.")
+
+#Step Twelve: Save, commit, push, and submit to Schoology.
