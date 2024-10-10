a3d0cffe1d68eaa87fee9a3ee2cd36abea2d08a9
Hasain Jinan <hjinan@brooklynsteamcenter.org>
2024-10-10 09:52:38 -0400

    Updated Lesson 12

--- Lessons/12_fantastic_functions.py
+++ Lessons/12_fantastic_functions.py
@@ -4,12 +4,18 @@ Write a function that takes a name as a
 parameter and prints a greeting message like "Hello, [name]!".
 """
 
+#Asks for name.
+you = input("What is your name: ")
+
 #Greeting function.
 def greeting(name):
+    """
+    Takes an argument called name and inputs the argument into the string "Hello {0}!"
+    """
     print("Hello " + name + "!")
 
 #Function.
-greeting("Hasain")
+greeting(you)
 
 """
 Task 2: Square of a Number
@@ -21,6 +27,9 @@ num = int(input("Enter an integer: "))
 
 #Square root function.
 def square():
+    """
+    Takes a input called num and squares it, returning the final value to a variable called "sqrt".
+    """
     return num**2
     
 #Returning square root.
@@ -37,9 +46,14 @@ example = int(input("Enter a number: "))
 
 #Function to determine if number is even or odd.
 def even_or_odd():
+    """
+    If the inputted integer returns 0 when you perform "[input] % 2," the number is even and the function returns the boolean value "True."
+
+    If the inputted integer returns 1 when you perform "[input] % 2," the number is odd and the function returns the boolean value "False."
+    """
     if example % 2 == 0:
         return True
-    elif example % 2 ==1:
+    elif example % 2 == 1:
         return False
 
 #Returning evaluation.
@@ -56,6 +70,11 @@ w = float(input("Enter the width of the rectangle: "))
 
 #Function to calculate area.
 def area():
+    """
+    The area of a rectangle is determined by the length of the rectangle multiplied by its width.
+
+    The function takes two previously asked for variables called "l [Length]" and "w [Width]" and multiplies them.
+    """
     return l * w
 
 #Returning area of rectangle.
@@ -72,6 +91,11 @@ deg_c = float(input("Enter temperature in degrees Celsius: "))
 
 #Function converting to Fahrenheit because 'Murica.
 def C_to_F():
+    """
+    Celsius is converted to Fahrenheit through the formula "C * (9/5) + 32 = F."
+    
+    C represents the inputted value of degrees Celsius, and F represents the output of degrees Fahrenheit.
+    """
     return (deg_c * 9/5) + 32
 
 #Returning temperature in Fahrenheit.
@@ -104,11 +128,20 @@ while choice == "Exit":
 
 #Function to find average.
 def avg():
+    """
+    If the length of the list is greater than 0, meaning the list contains numbers, then each item in the list is added to a value called total.
+
+    After that, the final value of total is divided by the length of the list which is found using len(list) and the final value is returned to the variable "average".
+
+    If the length of the list is equal to 0, meaning the list is empty, then the function simply returns the string "Empty list."
+    """
     total = 0
     for x in list:
         total = total + x
     if len(list) != 0:
         return total / len(list)
+    else:
+        return "Empty list."
 
 #Returning the mean.
 average = avg()
@@ -124,7 +157,7 @@ unit_price = float(input("Enter unit price: "))
 
 number = float(input("Enter quantity of items: "))
 
-#Asks if you have a discount and evaluates.
+#Asks if you have a discount and evaluates. Just making it convenient.
 dis = input("Do you have a discount? Yes/No: ")
 while dis.lower() != "yes" and dis.lower() != "no":
     print("Sorry I do not understand. Please try again.")
@@ -138,6 +171,13 @@ else:
 
 #Function to determine total price.
 def total(price, quantity, discount = 0):
+    """
+    If the discount amount is unaltered and is still 0, the function only multiplies the quantity by the unit price and returns that value as "total_price".
+
+    If the discount is not 0, then the function multiplies the price by the quantity before multiplying by the discount which will be a decimal.
+
+    Then the function returns the discounted price as "total_discount".
+    """
     if discount == 0:
         return price * quantity
     else:
@@ -147,6 +187,7 @@ def total(price, quantity, discount = 0):
 if dis.lower() == "no":
     total_price = total(unit_price, number)
     print(total_price)
+
 elif dis.lower() == "yes":
     total_discount = total(unit_price, number, dis_amount)
     print(total_discount)
